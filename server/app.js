b0d7afdd96705ab8814338acab976e6d0188e0c1
JGadi <johnpgadi@gmail.com>
2019-06-22 16:58:09 -0700

    modified app.js

--- server/app.js
+++ server/app.js
@@ -4,11 +4,16 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const MLAB_URI = process.env.MLAB_URI;
 
-mongoose.connect(`mongodb://${MLAB_URI}`, { useNewUrlParser: true, 
+mongoose.connect(`mongodb://${MLAB_URI}`, { useNewUrlParser: true , 
   auth: {
     user: process.env.MONGO_USER,
     password: process.env.MONGO_PASSWORD
-  }});
+  }},
+  (err) => {
+  if(err) {
+    return console.log(err, 'An internal server error has occured');
+  }
+}); 
 mongoose.Promise = Promise;
 
 const app = express();
