8a910633a0d99cdfa3e2f902d1763b028a212f2b
JGadi <johnpgadi@gmail.com>
2019-06-22 16:09:00 -0700

    modified port

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ const server = require('./app');
 require('dotenv').config();
 const PORT = process.env.PORT;
 
-server.listen(process.env.PORT, function() {
+server.listen(PORT, function() {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
